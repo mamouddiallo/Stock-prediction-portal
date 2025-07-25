ef3fa76517c7f896b9cde47620fa7f90d71c3f48
mamouddiallo <diallomamoud298@gmail.com>
2025-07-25 04:18:58 +0000

    Registration backend/api

--- stock_prediction_main/settings.py
+++ stock_prediction_main/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'account.apps.AccountConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
